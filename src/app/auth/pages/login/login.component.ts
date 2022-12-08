a1419a9294471e76f31b519dbced95d585836e32
Sergio De Luca <sdlbsso@gmail.com>
2022-12-08 00:38:22 -0300

    haciendo login

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { AuthResponse } from '../../interfaces/interfaces';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-login',
@@ -27,13 +28,20 @@ export class LoginComponent  {
 
  
   login(){
-    const {email, password} = this.miFormulario.value;
+     const {email, password} = this.miFormulario.value;
 
-    this.authService.login(email,password).subscribe((usr:AuthResponse)=>{
-      this.usr = usr;
-    })
-    
-    this.router.navigateByUrl('/dashboard');
+     this.authService.login(email,password)
+     .subscribe((ok)=>{
+        if(ok===true){
+           this.router.navigateByUrl('/dashboard');
+        }else{
+           Swal.fire({
+             title: ok,
+             icon: 'error',
+           })
+        }
+     })
+
   }
 
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 import { environment } from '../../../environments/environment.prod';
-import { AuthResponse } from '../interfaces/interfaces';
+import { AuthResponse, Usuario } from '../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -12,17 +13,43 @@ export class AuthService {
 
   private baseUrl: string = environment.baseUrl;
 
+  private _usuario!: Usuario;
+
+  get usuario(){
+    return {...this._usuario};
+  }
 
   constructor(private http: HttpClient) { }
 
 
-  login( email: string, password: string):Observable<AuthResponse>{
+  login( email: string, password: string){
     const url = `${this.baseUrl}/auth`
     const body = {
       email,
       password
     }
     return this.http.post<AuthResponse>(url,body)
+    .pipe(
+      tap( resp =>{
+        if(resp.ok){
+          localStorage.setItem('token',resp.token!);
+          this._usuario = {
+            name: resp.name!,
+            uid: resp.uid!,
+          }
+        }
+      }),
+      map( resp => resp.ok),
+      catchError(err=> of(err.error.msg))
+    )
+  }
+
+
+  validarToken(){
+    const url = `${this.baseUrl}/auth/renew`;
+    const headers = new HttpHeaders().set('x-token',localStorage.getItem('token') || '')
+    return this.http.get(url,{ headers });
+
   }
 
 
